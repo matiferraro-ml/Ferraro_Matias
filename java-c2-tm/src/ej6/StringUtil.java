89c67382cc8dd52287a47c7196227be64bfa133c
matiferraro-ml <matias.ferraro@mercadolibre.com>
2021-05-27 20:06:16 -0300

    Agrego ej6

--- java-c2-tm/src/ej6/StringUtil.java
+++ java-c2-tm/src/ej6/StringUtil.java
@@ -1,4 +1,4 @@
-package ejerc;
+package ej6;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -58,6 +58,46 @@ public class StringUtil {
 			arrNew[i] = (lpad(s, n, c));
 			i++;
 		}
+	}
+
+	// Idem lpad pero a la derecha
+	public static String rpad(String s, char c, int n) {
+		while(s.length() != n) {
+			s += c;
+		}
+		return s;
+	}
+
+	// Retorna una cadena idéntica a s pero sin espacios a la izquierda.
+	public static String ltrim(String s) {
+		return s.replaceAll("^\\s*","");
+	}
+
+	// Idem ltrim, pero sin espacios a la derecha.
+	public static String rtrim(String s) {
+		return s.replaceAll("\\s*$","");
+	}
+
+	// Idem lpad, pero sin espacios a derecha ni izquierda.
+	public static String trim(String s) {
+		return s.trim();
+	}
+
+	// Retorna la posición de la n-ésima ocurrencia del carácter c dentro de s, o -1 si s no contiene a c.
+	// Por ejemplo, si s = “John|Paul|George|Ringo”, c = ‘|’ y n=2, la función debe retornar la posición de la
+	// segunda ocurrencia del carácter ‘|’ (pipe) dentro de la cadena s. Que, en este caso, es: 9.
+	public static int indexOfN(String s, char c, int n) {
+		int i = 0;
+		int ind = -1;
+		while(i < n) {
+			ind = s.indexOf(c);
+			if(ind != -1) {
+				s = s.substring(ind + 1, s.length()-1);
+			}
+			else break;
+			i++;
+		}
+		return ind;
 	}
 
 }
